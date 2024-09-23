7f66a381897c781c8f066fa5ba95e480eb85c709
victor odambatafwa <omondivictoras@gmail.com>
2024-09-23 18:57:03 +0300

    Updated username and password in _config.js file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<Omondivictor120>:<6N0Kc4PdAR2UmeYN>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<Omondivictor120>:<6N0Kc4PdAR2UmeYN>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
